81320c2758b6950d414dcd194749daa1534b7a1f
unknown <aminekaramos@gmail.com>
2020-01-30 23:01:56 +0100

    modification module 1

--- app/src/main/java/com/example/zombiblio/Module1.java
+++ app/src/main/java/com/example/zombiblio/Module1.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -12,6 +14,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.squareup.picasso.Picasso;
 import android.content.Context;
+import android.widget.TextView;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
@@ -26,10 +30,23 @@ public class Module1 extends AppCompatActivity {
 
         ImageView question = (ImageView) findViewById(R.id.question);
 
+        new CountDownTimer(60000, 1000) {
+
+            TextView counter = (TextView)  findViewById(R.id.counter);
+
+            public void onTick(long millisUntilFinished) {
+                counter.setText("Temps restant: " + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                counter.setText("done!");
+            }
+        }.start();
+
         //question.setBackgroundColor(0xFF00FF00);
-        Picasso.get() // Context
-                .load("https://img.favpng.com/0/17/19/computer-icons-software-testing-icon-design-png-favpng-u2kV0w1yAF5YhWvyFE8T6Fmxm.jpg") // URL or file
-                .into(question); // An ImageView object to show the loaded image
+        //Picasso.get() // Context
+               // .load("https://img.favpng.com/0/17/19/computer-icons-software-testing-icon-design-png-favpng-u2kV0w1yAF5YhWvyFE8T6Fmxm.jpg")
+               // .into(question);
 
     }
 }
